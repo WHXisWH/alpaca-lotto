d162fb8b21f98f429d4b5e08cb824600ce38860a
WHXisWH <whx12110405@gmail.com>
2025-05-05 15:28:59 +0900

    fix abi

---
+++ frontend/src/constants/abi/index.ts
@@ -0,0 +1,5 @@
+export { default as AlpacaLottoAbi } from './AlpacaLotto.json';
+export { default as SessionKeyManagerAbi } from './SessionKeyManager.json';
+export { default as SocialRecoveryModuleAbi } from './SocialRecoveryModule.json';
+export { default as TestToken1Abi } from './TestToken1.json';
+export { default as TestToken2Abi } from './TestToken2.json';
