788e30b0ea4cb9313a5f35b5905f754ec1d944cb
WHXisWH <whx12110405@gmail.com>
2025-05-10 02:26:11 +0900

    update

--- frontend/src/constants/abi/index.ts
+++ frontend/src/constants/abi/index.ts
@@ -1,6 +1,6 @@
+// frontend/src/constants/abi/index.ts
 export { default as AlpacaLottoAbi } from './AlpacaLotto.json';
 export { default as SessionKeyManagerAbi } from './SessionKeyManager.json';
-export { default as SocialRecoveryModuleAbi } from './SocialRecoveryModule.json';
 export { default as TestToken1Abi } from './TestToken1.json';
 export { default as TestToken2Abi } from './TestToken2.json';
 export { default as EntryPointAbi } from './EntryPoint.json';
@@ -55,4 +55,4 @@ export const ERC20Abi = [
     stateMutability: 'nonpayable',
     type: 'function',
   }
-] as const; // Add as const assertion for wagmi v2
+] as const;
